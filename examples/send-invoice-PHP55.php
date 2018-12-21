d0c8efdfa758a4ef0f33864faae21e9f91a55c4f
aded <aded@aded.it>
2018-12-21 12:48:37 +0100

    Adds example for PHP 5.5+

---
+++ examples/send-invoice-PHP55.php
@@ -0,0 +1,41 @@
+<?php
+
+ini_set("soap.wsdl_cache_enabled", 0);
+ini_set('soap.wsdl_cache_ttl', 0);
+
+use \Taocomp\Einvoicing\Sdicoop\Client;
+use \Taocomp\Einvoicing\Sdicoop\FileSdIBase;
+use \Taocomp\Einvoicing\Sdicoop\RispostaSdIRiceviFile;
+
+try
+{
+    require_once(__DIR__ . '/../autoload.php');
+
+    // Set certs and key
+    Client::setPrivateKey(__DIR__ . '/../assets/key/client.key');
+    Client::setClientCert(__DIR__ . '/../assets/certs/client.pem');
+    Client::setCaCert(__DIR__ . '/../assets/certs/ca.pem');
+
+    $client = new Client(array(
+        'endpoint' => 'https://testservizi.fatturapa.it/ricevi_file',
+        'wsdl'     => __DIR__ . '/../assets/wsdl/SdIRiceviFile_v1.0.wsdl'
+    ));
+    
+    // Verbose (default: false)
+    // Client::$verbose = false;
+    
+    $fileSdI = new FileSdIBase();
+    $fileSdI->load(__DIR__ . '/invoice.xml');
+    $response = new RispostaSdIRiceviFile($client->RiceviFile($fileSdI));
+
+    // Process response:
+    // -----------------------------------------
+    // $id       = $response->IdentificativoSdI;
+    // $datetime = $response->DataOraRicezione;
+    // $error    = $response->Errore;
+    // -----------------------------------------
+}
+catch (\Exception $e)
+{
+    Client::log($e->getMessage(), LOG_ERR);
+}
