847245e38426d268b58a19fe497840edaed6e5c2
aded <aded@aded.it>
2018-12-21 22:56:28 +0100

    Removes derived client classes and old autoload.php

--- src/SdiRiceviNotifica.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Taocomp\Einvoicing\Sdicoop;
-
-class TestSdiRiceviNotifica extends Client
-{
-    const ENDPOINT = 'https://servizi.fatturapa.it/ricevi_notifica';
-    const WSDL     = __DIR__ . '/../assets/wsdl/SdIRiceviNotifica_v1.0.wsdl';
-}
